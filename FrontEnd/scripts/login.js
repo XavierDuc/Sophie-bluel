8effffc810d555d0931b6a26cf6da3d601abb486
ZoonyMac <zoonymac@ZoonyMacs-MacBook-Pro.local>
2024-08-28 18:33:49 +0200

    modifications login js .then response et data

--- FrontEnd/scripts/login.js
+++ FrontEnd/scripts/login.js
@@ -1,27 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
   const loginForm = document.getElementById("login-form");
 
-  loginForm
-    .addEventListener("submit", function (event) {
-      event.preventDefault();
+  loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-      const email = document.getElementById("email").value;
-      const password = document.getElementById("password").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-      fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: {
-          accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
-      }).then((response) => {
-        return response.json();
-      });
-      window.location.href = "index.html";
+    fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
     })
-    .catch((error) => {
-      // Affichage du message d'erreur
-      alert(error.message);
-    });
+      .then((response) => {
+        if (response.status === 404) {
+          throw new Error("Utilisateur non trouvé");
+        }
+        if (response.status === 401) {
+          throw new Error("Non autorisé");
+        }
+        if (!response.ok) {
+          throw new Error("Erreur lors de la connexion");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        localStorage.setItem("token", data.token);
+
+        localStorage.setItem("userId", data.userId);
+        // Redirection vers la page d'accueil
+        window.location.href = "index.html";
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  });
 });
